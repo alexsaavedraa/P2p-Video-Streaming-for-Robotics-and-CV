5df0910cf61ddce2bdd98305c10e6381e4e6a87d
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2024-01-27 17:02:01 -0600

    basic video player

--- src/client.py
+++ src/client.py
@@ -5,20 +5,22 @@ from aiortc import (RTCPeerConnection,
 from aiortc.contrib.media import MediaRecorder
 import asyncio
 
+from video_player import VideoStreamPlayer
+
 HOST = "127.0.0.1"
 PORT = 1234
 
-async def run_client(signaling, pc, recorder):
+async def run_client(signaling, pc, player):
     @pc.on("track")
     def on_track(track):
         print("Receiving %s" % track.kind)
-        recorder.addTrack(track)
+        player.addTrack(track)
 
     while True:
         obj = await signaling.receive()
         if isinstance(obj, RTCSessionDescription):
                 await pc.setRemoteDescription(obj)
-                await recorder.start()
+                await player.start()
                 if obj.type == "offer":
                     await pc.setLocalDescription(await pc.createAnswer())
                     await signaling.send(pc.localDescription)
@@ -30,19 +32,19 @@ if __name__ == "__main__":
     print("Initializing Client...")
     signaling = TcpSocketSignaling(host=HOST, port=PORT)
     pc = RTCPeerConnection()
-    recorder = MediaRecorder("video.mp4")
+    player = VideoStreamPlayer()
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(
             run_client(
                 signaling,
                 pc,
-                recorder
+                player
             )
         )
     except KeyboardInterrupt:
         pass
     finally:
-        loop.run_until_complete(recorder.stop())
+        loop.run_until_complete(player.stop())
         loop.run_until_complete(signaling.close())
         loop.run_until_complete(pc.close())
---
+++ src/video_player.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy
+import asyncio
+from aiortc.contrib.media import (MediaStreamTrack,
+                                  MediaStreamError)
+
+class VideoStreamPlayer():
+    def addTrack(self, track: MediaStreamTrack):
+        self.track = track
+
+    async def start(self):
+        print("Starting stream")
+        asyncio.ensure_future(self.__run_track())
+
+    async def stop(self):
+        print("Stopping stream")
+        cv2.destroyAllWindows()
+
+    async def __run_track(self):
+        frame = True
+        i = 0
+        while frame:
+            try:
+                frame = await self.track.recv()
+                print("Received frame", i)
+                i+=1
+            except MediaStreamError:
+                return
+            cv2.imshow('Frame', frame.to_ndarray()) 
+        
