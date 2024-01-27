e3c137b310bedec4194dbc2a8982707d376a5c5d
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2024-01-27 13:40:08 -0600

    server and client init

--- src/client.py
+++ src/client.py
@@ -1,1 +1,24 @@
 from aiortc.contrib.signaling import TcpSocketSignaling
+from aiortc import (RTCPeerConnection, 
+                    VideoStreamTrack)
+import asyncio
+
+HOST = "127.0. 0.1"
+PORT = 8080
+
+async def run_client(signaling, pc):
+    obj = await signaling.recieve()
+    return obj
+
+if __name__ == "__main__":
+    print("Initializing Server...")
+    signaling = TcpSocketSignaling(host=HOST, port=PORT)
+    pc = RTCPeerConnection()
+    loop = asyncio.get_event_loop()
+    obj = loop.run_until_complete(
+        run_client(
+            signaling, 
+            pc
+        )
+    )
+    print(obj)
--- src/server.py
+++ src/server.py
@@ -1,23 +1,26 @@
 from aiortc.contrib.signaling import TcpSocketSignaling
-from aiortc import RTCPeerConnection
+from aiortc import (RTCPeerConnection, 
+                    VideoStreamTrack)
 import asyncio
 
 HOST = "127.0. 0.1"
 PORT = 8080
 
-async def run_server(signaling, peer_connection, media, data_channel):
-    await peer_connection.createOffer(media, data_channel)
+async def run_server(signaling, pc):
+    await signaling.connect()
+    await pc.setLocalDescription(await pc.createOffer())
+    await signaling.send(pc.localDescription)
 
 if __name__ == "__main__":
     print("Initializing Server...")
     signaling = TcpSocketSignaling(host=HOST, port=PORT)
-    peer_connection = RTCPeerConnection()
+    pc = RTCPeerConnection()
     loop = asyncio.get_event_loop()
+    track = VideoStreamTrack()
+    pc.addTrack(track)
     loop.run_until_complete(
         run_server(
             signaling, 
-            peer_connection, 
-            media,
-            data_channel
+            pc
         )
     )
