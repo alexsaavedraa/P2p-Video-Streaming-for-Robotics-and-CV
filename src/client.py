66f32e44036c49da724fa18feb1a9b765175325a
Alex Saavedra <alexsaavedra2023@u.northwestern.edu>
2024-01-27 21:54:17 -0600

    client working

--- src/client.py
+++ src/client.py
@@ -1,21 +1,56 @@
-from aiortc.contrib.signaling import TcpSocketSignaling
-from aiortc import RTCPeerConnection
+from aiortc.contrib.signaling import TcpSocketSignaling, BYE
+from aiortc import RTCPeerConnection, RTCSessionDescription, RTCIceCandidate, VideoStreamTrack
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
 import asyncio
 import socket
 
 HOST = "127.0.0.1"
-PORT = 8080
+PORT = 1234
 
-async def run_client(signaling):
-    obj = await signaling.receive()
-    print(obj)
+async def run_client(signaling, pc, recorder):
+    pc.addTrack(VideoStreamTrack())
+
+    @pc.on("track")
+    def on_track(track):
+        print("Receiving %s" % track.kind)
+        recorder.addTrack(track)
+    while True:
+        obj = await signaling.receive()
+        if isinstance(obj, RTCSessionDescription):
+                
+                await pc.setRemoteDescription(obj)
+                await recorder.start()
+                #client recieves an offer
+                if obj.type == "offer":
+                    # send answer
+                    await pc.setLocalDescription(await pc.createAnswer())
+                    await signaling.send(pc.localDescription)
+        elif isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
+
+        #print(obj)
 
 if __name__ == "__main__":
     print("Initializing Client...")
     signaling = TcpSocketSignaling(host=HOST, port=PORT)
+    pc = RTCPeerConnection()
+    recorder = MediaRecorder("video.mp4")
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        run_client(
-            signaling, 
+    try:
+        loop.run_until_complete(
+            run_client(
+                signaling,
+                pc,
+                recorder
+            )
         )
-    )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # cleanup
+        loop.run_until_complete(recorder.stop())
+        loop.run_until_complete(signaling.close())
+        loop.run_until_complete(pc.close())
--- src/server.py
+++ src/server.py
@@ -1,27 +1,50 @@
-from aiortc.contrib.signaling import TcpSocketSignaling
+from aiortc.contrib.signaling import TcpSocketSignaling, BYE
 from aiortc import (RTCPeerConnection, 
-                    VideoStreamTrack)
+                    VideoStreamTrack, RTCIceCandidate)
 import asyncio
 import socket
-
 HOST = "127.0.0.1"
-PORT = 8080
+PORT = 1234
+import numpy
+import cv2
+import math
+from av import VideoFrame
+
+
 
 async def run_server(signaling, pc):
+
+    pc.addTrack(VideoStreamTrack())
     await signaling.connect()
     await pc.setLocalDescription(await pc.createOffer())
     await signaling.send(pc.localDescription)
+    while True:
+        obj = await signaling.receive()
+        if isinstance(obj, RTCIceCandidate):
+            await pc.addIceCandidate(obj)
+        elif obj is BYE:
+            print("Exiting")
+            break
+    
+    
+
+        
 
 if __name__ == "__main__":
     print("Initializing Server...")
     signaling = TcpSocketSignaling(host=HOST, port=PORT)
     pc = RTCPeerConnection()
     loop = asyncio.get_event_loop()
-    track = VideoStreamTrack()
-    pc.addTrack(track)
-    loop.run_until_complete(
-        run_server(
-            signaling, 
-            pc
+    try:
+        loop.run_until_complete(
+            run_server(
+                signaling, 
+                pc
+            )
         )
-    )
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # cleanup
+        loop.run_until_complete(signaling.close())
+        loop.run_until_complete(pc.close())
