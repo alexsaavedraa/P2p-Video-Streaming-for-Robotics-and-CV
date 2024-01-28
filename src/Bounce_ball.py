557ea1190fe285b23f6bffbeba19b70826012bb9
Alex Saavedra <alexsaavedra2023@u.northwestern.edu>
2024-01-27 23:14:56 -0600

    ball bounce continuous

--- src/Bounce_ball.py
+++ src/Bounce_ball.py
@@ -23,24 +23,23 @@ class Bounce_ball(VideoStreamTrack):
     def __init__(self):
         super().__init__()  # don't forget this!
         self.counter = 0
-        
+        self.frames = []
         self.x = 100
         self.y = 100
         self.dx = 5
         self.dy = -5
+        for i in range(30):
+            self.frames.append(self.get_next_frame())
 
         
     def get_next_frame(self):
-        img = np.zeros((X,Y,3),dtype='uint8')
+        img = np.zeros((W,L,3),dtype='uint8')
         
     
-        # Display the image
         img = np.zeros((L,W,3),dtype='uint8') 
-        # Increment the position
         self.x = self.x+self.dx
         self.y = self.y+self.dy
         cv2.circle(img,(self.x,self.y),20,(255,0,0),-1)
-        # Change the sign of increment on collision with the boundary
         if self.y >=L:
             self.dy *= -1
         elif self.y<=0:
@@ -56,13 +55,13 @@ class Bounce_ball(VideoStreamTrack):
 
 
     async def recv(self):
+        
         pts, time_base = await self.next_timestamp()
 
-        frame = self.frames[self.counter % 30]
+        frame = self.get_next_frame()
         frame.pts = pts
         frame.time_base = time_base
         self.counter += 1
-        print(self.counter)
         return frame
 
 
