74c44d1bdec63098340ff0346b874c1ef7f97758
Alex Saavedra <alexsaavedra2023@u.northwestern.edu>
2024-01-27 23:04:00 -0600

    ball Bounce in progress

--- src/Bounce_ball.py
+++ src/Bounce_ball.py
@@ -31,28 +31,27 @@ class Bounce_ball(VideoStreamTrack):
 
         
     def get_next_frame(self):
-        res = []
         img = np.zeros((X,Y,3),dtype='uint8')
-        dx,dy =5,5
-        for i in range(600):
-            # Display the image
-            img = np.zeros((480,640,3),dtype='uint8') 
-            # Increment the position
-            x = x+dx
-            y = y+dy
-            cv2.circle(img,(x,y),20,(255,0,0),-1)
-            # Change the sign of increment on collision with the boundary
-            if self.y >=L:
-                dy *= -1
-            elif y<=0:
-                dy *= -1
-            if x >=W:
-                dx *= -1
-            elif x<=0:
-                dx *= -1
-            ##apend the frame
-            res = VideoFrame.from_ndarray(
-                        img, format="bgr24")
+        
+    
+        # Display the image
+        img = np.zeros((L,W,3),dtype='uint8') 
+        # Increment the position
+        self.x = self.x+self.dx
+        self.y = self.y+self.dy
+        cv2.circle(img,(self.x,self.y),20,(255,0,0),-1)
+        # Change the sign of increment on collision with the boundary
+        if self.y >=L:
+            self.dy *= -1
+        elif self.y<=0:
+            self.dy *= -1
+        if self.x >=W:
+            self.dx *= -1
+        elif self.x<=0:
+            self.dx *= -1
+        
+        res = VideoFrame.from_ndarray(
+                    img, format="bgr24")
         return res
 
 
